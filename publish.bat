52060a189e21b38028541bf997bf656fe5bcce97
Doridian <git@doridian.net>
2024-03-30 12:40:57 -0700

    ignore

---
+++ publish.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad" create -folder .\ -out ..\spacebuild_colliders.gma
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish" update -id 2387054596 -addon ..\spacebuild_colliders.gma -icon icon.jpg
+del  ..\spacebuild_colliders.gma
